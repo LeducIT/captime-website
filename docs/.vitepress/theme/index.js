952ba185ebb0a959627a0fbaea9f53ed49ef061b
martindonadieu <martindonadieu@gmail.com>
2021-12-05 19:46:31 +0000

    add base landing

--- docs/.vitepress/theme/index.js
+++ docs/.vitepress/theme/index.js
@@ -1,7 +1,9 @@
 import "./main.css";
 
 import DefaultTheme from 'vitepress/theme'
+import Layout from './layouts/Layout.vue'
 
+DefaultTheme.Layout = Layout
 export default {
   ...DefaultTheme
 }
---
+++ docs/.vitepress/theme/layouts/Layout.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import Footer from '../components/Footer.vue'
+import Header from '../components/Header.vue'
+
+// import 'virtual:windi.css'
+// import 'virtual:windi-devtools'
+</script>
+<template>
+  <Header/>
+  <Content /><!-- this is where markdown content will be rendered -->
+  <Footer/>
+</template>
