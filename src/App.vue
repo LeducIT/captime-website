71cde0b84488eeb7cba22fa5d0cbd73d3297aa1e
martindonadieu <martindonadieu@gmail.com>
2022-06-03 18:47:09 +0100

    fix: description length

--- src/App.vue
+++ src/App.vue
@@ -4,15 +4,16 @@ const router = useRouter()
 // https://github.com/vueuse/head
 // you can use this to manipulate the document head in any components,
 // they will be rendered correctly in the html results with vite-ssg
+const desc = 'The best crossfit timer you can find for coaches and athletes. Available in iOS and Android | Do your TABATA, AMRAP, EMOM, HIIT, Interval, For Time (with voice command), SALLY, ROXANNE BURPEES, FIGHT GONE BAD, Video record for CrossFit games in one app !'
 useHead({
   title: 'Captime | Crossfit timer',
   meta: [
-    { name: 'description', content: 'The best crossfit timer you can find. Available in iOS and Android.' },
+    { name: 'description', content: desc },
     { name: 'og:type', content: 'website' },
     { name: 'og:title', content: 'Captime | Crossfit timer' },
     { name: 'twitter:title', content: 'Captime | Crossfit timer' },
     { name: 'twitter:card', content: 'summary_large_image' },
-    { name: 'twitter:description', content: 'The best crossfit timer you can find. Available in iOS and Android.' },
+    { name: 'twitter:description', content: desc },
     { name: 'twitter:domain', content: import.meta.env.domain },
     { name: 'twitter:image', content: `https://${import.meta.env.domain}/alternative_to_1.webp` },
     { name: 'og:image:alt', content: 'Crossfit Time App illustration' },
