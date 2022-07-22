70a61c3795bb6ad768de2215c088d4cca70899ec
martindonadieu <martindonadieu@gmail.com>
2022-07-22 18:47:08 +0100

    fix: use pnpm

---
+++ server/routes/sitemap.xml.ts
@@ -0,0 +1,30 @@
+import { SitemapStream, streamToPromise } from 'sitemap'
+import { serverQueryContent } from '#content/server'
+
+
+export default defineEventHandler(async (event) => {
+  try {
+    const config = useRuntimeConfig()
+    const smStream = new SitemapStream({ hostname: config.baseUrl })
+
+    smStream.write({ url: '/blog', changefreq: 'daily', priority: 1 })
+
+    const blogs = await serverQueryContent(event).where({ published: true }).find()
+
+    blogs.forEach((article: any) => {
+      smStream.write({
+        url: `/blog/${article.slug}`,
+        changefreq: 'daily',
+        priority: 0.5,
+      })
+    })
+    smStream.end()
+    const data = await streamToPromise(smStream)
+    event.res.setHeader('Content-Type', "application/xml")
+    return data.toString()
+  } catch (e) {
+    console.error(e)
+    event.res.statusCode = 500
+    return {}
+  }
+})
