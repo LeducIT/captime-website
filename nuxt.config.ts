708af16021c46fac3a3d5515846bcdf580e0f85a
Martin Donadieu <martindonadieu@gmail.com>
2023-08-06 17:18:10 +0100

    fix: remove preset egde it don't work with it

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -119,7 +119,7 @@ export default defineNuxtConfig({
         target: 'esnext',
       },
     },
-    preset: 'netlify-edge',
+    // preset: 'netlify-edge',
     prerender: {
       routes: ['/', '/robots.txt'],
     },
